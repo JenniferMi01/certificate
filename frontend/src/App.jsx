b9b58b7ff584e0596e38ccf6cc95f8608c057ba8
Michella Jen <rinasoajenny@gmail.com>
2025-11-25 11:01:50 +0300

    feat: implement routing in App component and update main entry point

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,40 @@
-import './App.css'
-import Demo from './components/login'
+// import './App.css'
+// import Demo from './components/login'
+
+// function App() {
+//   return (
+//     <div >
+//       <Demo />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// 14/11/2025
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/login';
+import Home from './pages/home'; // CHANGÉ : ./components → ./pages
+import { PrivateRoute } from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="bg-purple-600 h-screen flex items-center justify-center">
-      <Demo />
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Ajoute le Router
+import './index.css';
+import App from './App.jsx';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MantineProvider>
-      <App />
+      <Router> {/* Enveloppe ton App avec Router */}
+        <App />
+      </Router>
     </MantineProvider>
   </StrictMode>,
 );
