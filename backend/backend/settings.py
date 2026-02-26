13ab9c71c71bab6849edc8e9f39eeb1c22e33bfe
Alain Nambii <alainnambi@gmail.com>
2026-02-26 14:54:25 +0100

    feat: add Docker support with Dockerfiles for frontend and backend, configure docker-compose, and update .gitignore files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/' le tena izy 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
